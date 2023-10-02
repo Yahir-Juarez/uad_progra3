3634f1f98a4feeb662cd4e8c0ac1c5901ab4126b
Yahir-Juarez <idv22c.yjuarez@uartesdigitales.edu.mx>
2023-10-01 21:42:33 -0600

    Se agrego en el parcial-1 el parser del proyecto
    Tambien en el mismo se dejaron los documentos para que se pueda ver la demostracion de como funciona, y que se imprima lo que se guarde en las estructuras de datos aunque no supe como entrar mas adentro de los archivos para abrirlos.

--- uad_progra3/uad_progra3.cpp
+++ uad_progra3/uad_progra3.cpp
@@ -11,11 +11,14 @@ using namespace std;
 #include "Include/CAppObjLoader.h"
 #include "Include/CAppGeometricFigures.h"
 #include "Include/CAppEmpty.h"
+#include "Parcial-1.h"
 
 int main()
 {
+	archivos_obj nuevo_archivo;
 	CApp *app = NULL;					// Pointer to BASE class CApp
-	app = new CAppEmpty(800, 600);		// Using pointer to base class, create a new object of DERIVED class
+	//app = new CAppEmpty(800, 600);		// Using pointer to base class, create a new object of DERIVED class
+	app = new CAppCubeTest(800, 600);
 	app->run();							// Run the app
 	delete app;							// Delete pointer
 	app = NULL;							// Set pointer to NULL
